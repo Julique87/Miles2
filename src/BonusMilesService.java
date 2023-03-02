68d4868864000c30c5dacf435a08927c1588324d
Julia Novikova <julikova.n@gmail.com>
2023-03-02 14:45:16 +0300

    Reformat commit

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -3,10 +3,10 @@ public class BonusMilesService {
     public int calculate(int cost) {
         int result;
         if (cost >= 20) {
-            result = cost / 20; }
-           else {
-                result = 0;
-            }
+            result = cost / 20;
+        } else {
+            result = 0;
+        }
 
         return result;
     }
